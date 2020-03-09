206e91aca7b5645f4d56ec7c3543f518c92151d4
blackrose9 <sarah.mwendwa@gmail.com>
2020-03-09 17:21:06 +0300

    add unsolved Kata for removing extra zeroes at the end of a number

---
+++ RandomToyChallenges/eradicatezeroes.js
@@ -0,0 +1,7 @@
+//take in number
+//split number into array
+// reverse array
+// loops through each number checking if it is not 0
+// splice at number !=0
+// store the spliced rest of the array
+// reverse the number again and return it
