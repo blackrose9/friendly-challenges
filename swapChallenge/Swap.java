267721c45fd5bea29edb00c5cf87acfcb663d3b6
blackrose9 <sarah.mwendwa@gmail.com>
2020-03-28 14:09:03 +0300

    Add minimum swaps challenge java and js incomplete solution

---
+++ swapChallenge/Swap.java
@@ -0,0 +1,40 @@
+package swapChallenge;
+
+import java.lang.reflect.Array;
+
+class Swap {
+   public static void main(String[] args) {
+      
+   }
+   public static Array needSwap(Array array){
+         
+         return array;
+   }
+}
+
+/*
+class GfG  {
+      // Function returns the minimum number of swaps
+            // required to sort the array
+                  public static int minSwaps(int[] arr)      {
+                            int n = arr.length;
+                            // Create two arrays and use as pairs where first
+                            // array is element and second array
+                            // is position of first element
+                                                ArrayList <Pair <Integer, Integer> > arrpos = new ArrayList <Pair <Integer, Integer> > ();
+                                                          for (int i = 0; i < n; i++)
+                                                          afunction bubbleSort(items) {
+                                                               var length = items.length;
+                                                               var swaps;
+                                                                    for (var i = 0; i < length; i++) {
+                                                                     //Number of passes
+                                                                            for (var j = 0; j < (length - i - 1); j++) {
+                                                                             //Notice that j < (length - i)
+                                                                             //Compare the adjacent positions
+                                                                             if (items[j] > items[j + 1]) {
+                                                                             swaps++
+                                                                             //Swap the numbers
+                                                                             var tmp = items[j];
+                                                                             //Temporary variable to hold the current number
+                                                                             items[j] = items[j + 1]; //Replace current number with adjacent numb
+*/
---
+++ swapChallenge/swap.js
@@ -0,0 +1,5 @@
+needSwap = (arr) => {
+   //debugger;
+      let sortedArr = sort((a,b) => a-b);
+   
+   console.log(needSwap([7, 1, 3, 2, 4, 5, 6]));
