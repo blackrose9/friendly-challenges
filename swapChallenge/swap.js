50316ced731b1465a7e4f87b6ae2dabdf9d53107
blackrose9 <sarah.mwendwa@gmail.com>
2020-03-28 14:10:59 +0300

    add closing curly bracket to needSwap function.

--- swapChallenge/swap.js
+++ swapChallenge/swap.js
@@ -2,4 +2,5 @@ needSwap = (arr) => {
    //debugger;
       let sortedArr = sort((a,b) => a-b);
    
-   console.log(needSwap([7, 1, 3, 2, 4, 5, 6]));
+   console.log(needSwap([7, 1, 3, 2, 4, 5, 6]))
+};
