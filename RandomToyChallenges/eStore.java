935a81b260f0b9e43cdea371f455c7cc96e41db1
blackrose9 <sarah.mwendwa@gmail.com>
2020-03-28 14:07:59 +0300

    Add eStore interview challenge, incomplete solution Java and JS

---
+++ RandomToyChallenges/eStore.java
@@ -0,0 +1,17 @@
+package RandomToyChallenges;
+
+import java.util.List;
+
+class eStore {
+   public static String featuredProduct(List<String> products) {
+      //if statement to add the occurences of the items object key value
+   
+      //order the items by occurences
+   
+      //order highest occurence alphabetically
+   
+      //print item at index.length-1
+      return null;
+      }
+}
+
---
+++ RandomToyChallenges/eStore.js
@@ -0,0 +1,19 @@
+function featuredProduct(products) {
+   // Write your code here
+   products.sort();
+   let isProduct = []
+   let productOccur = {}
+   products.forEach(product => {
+       if(!(isProduct.includes(product))){
+           isProduct.push(product);
+           productOccur[product] = 1;
+       }
+       else{
+           productOccur[product] = [key+1];
+       }
+   });
+   Object.keys(productOccur).sort((a,b)=>a-b).forEach(function(key){
+       if(productOccur.key){}
+   })
+}
+    featuredProduct("yellowShirt","redHat","blackShirt","bluePants","redHat","pinkHat","blackShirt","yellowShirt","greenPants","greenPants","greenPants")
