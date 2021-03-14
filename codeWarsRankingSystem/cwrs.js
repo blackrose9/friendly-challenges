a37f43c107d6b53302fb97ef18b1d22586540ef1
blackrose9 <sarah.mwendwa@outlook.com>
2021-03-14 22:44:12 +0200

    Add new codewars ranking challenge. Create some functions to start building the program.

---
+++ codeWarsRankingSystem/cwrs.js
@@ -0,0 +1,17 @@
+const startRank = -8;
+var userCurrentRank;
+
+User = () => {
+
+}
+
+checkIfRankZero = (userCurrentRank) => {
+   if(userCurrentRank = 0){
+      userCurrentRank+1;
+   }
+   return userCurrentRank;
+}
+
+RankUp = (userCurrentRank, kataRank) => {
+
+}
