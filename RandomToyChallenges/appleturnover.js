ae6d81804995230e9263252474fa0580610646e3
blackrose9 <sarah.mwendwa@outlook.com>
2021-08-11 16:39:14 +0200

    This commit adds a new solution for the A.P. II 8kyu challenge

---
+++ RandomToyChallenges/appleturnover.js
@@ -0,0 +1,4 @@
+apple = (x) => {
+   return (Math.pow(x,2) > 1000)? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+ }
+//  returns a witty Alan Patridge remark if the value of x-squared is higher than 1000
